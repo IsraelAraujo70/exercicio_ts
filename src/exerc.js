3a69650f5c021c4719c7fdc6c5c9a7962debde0e
IsraelAraujo70 <israelaraujodeoliveira@gmail.com>
2024-10-21 14:17:18 -0300

    First commit

---
+++ src/exerc.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudar(a) {
+    return `Olá ${a}, seja bem-vindo ao meu GitHub`;
+}
+console.log(multiplicar(6, 5));
+console.log(saudar('Israel'));
---
+++ src/exerc.ts
@@ -0,0 +1,9 @@
+function multiplicar(a:number,b:number): number{
+    return a*b
+}
+function saudar(a:string):string{
+    return `Olá ${a}, seja bem-vindo ao meu GitHub`
+}
+
+console.log(multiplicar(6,5))
+console.log(saudar('Israel'))
